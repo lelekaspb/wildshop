d67793c4195066c68d2d3a65871d219fbfb2cfd5
lelekaspb <bov-1986@yandex.ru>
2023-12-19 22:46:44 +0100

    completed contact page

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,20 +1,44 @@
 import { getCompanyGeneralInformation } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+import styles from "./page.module.css";
 
 export default async function Contact() {
   const companyInfo = await getCompanyGeneralInformation();
   return (
-    <main>
-      <h2>Contact</h2>
-      <p>Phone: {companyInfo.phone}</p>
-      <p>Email: {companyInfo.email}</p>
-      <p>Instagram: {companyInfo.instagram}</p>
-      <div>
-        Address: <PortableText value={companyInfo.address} />
-      </div>
-      <div>
-        Working hours: <PortableText value={companyInfo.workingHours} />
-      </div>
-    </main>
+    <div className={styles.contact_page}>
+      <section className={styles.heading}>
+        <h1 className={styles.heading_text}>Kontakt</h1>
+      </section>
+      <section className={styles.instagram}>
+        <h2 className={styles.section_heading}>Instagram</h2>
+        <p className={styles.section_text}>
+          Find os på Instagram under navnet {companyInfo.instagram}
+        </p>
+      </section>
+      <section className={styles.phone}>
+        <h2 className={styles.section_heading}>Telefon</h2>
+        <p className={styles.section_text}>
+          Ring til os på telefonnumer {companyInfo.phone}
+        </p>
+      </section>
+      <section className={styles.email}>
+        <h2 className={styles.section_heading}>Email</h2>
+        <p className={styles.section_text}>
+          Skriv en mail på {companyInfo.email}. Vi svarer så hurtigt som muligt.
+        </p>
+      </section>
+      <section className={styles.address}>
+        <h2 className={styles.section_heading}>Adresse</h2>
+        <div className={styles.section_text}>
+          <PortableText value={companyInfo.address} />
+        </div>
+      </section>
+      <section className={styles.working_hours}>
+        <h2 className={styles.section_heading}>Åbningstider</h2>
+        <div className={styles.section_text}>
+          <PortableText value={companyInfo.workingHours} />
+        </div>
+      </section>
+    </div>
   );
 }
